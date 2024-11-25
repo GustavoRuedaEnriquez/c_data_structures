2c8ef47b7e8ac2d48a5bf4122f7a9acba6c3eaf1
GustavoRuedaEnriquez <grueda-personal@outlook.com>
2024-11-24 21:02:55 -0600

    Add support for unsigned long and long data types

--- queue/usage_queue.c
+++ queue/usage_queue.c
@@ -28,6 +28,16 @@ int main() {
 
   printf("\n\n\n");
 
+  printf("====== Testing queue for unsigned longs ======\n");
+  test_ulong();
+
+  printf("\n\n\n");
+
+  printf("====== Testing queue for longs ======\n");
+  test_long();
+
+  printf("\n\n\n");
+
     return 0;
 }
 
@@ -84,8 +94,69 @@ void test_int() {
   queue_destroy(queue_int);
 }
 
-void test_ulong();
-void test_long();
+void test_ulong()
+{
+  Queue_t queue_ulong = queue_create(ULONG); 
+  unsigned long n = 429496729;
+  unsigned long temp = n;
+
+  printf("\n*** Adding 10 unsigned long elements to the queue ***\n");
+  for(unsigned long i = 1; i <= 10; i++) {
+    temp = n * i;
+    queue_offer(queue_ulong, (void *)&temp);
+  }
+  queue_print(queue_ulong);
+
+  printf("\n*** Peeking queue's front ***\n");
+  type_t first = queue_peek(queue_ulong);
+  printf("Peeking queue's first element: %lu\n", *(unsigned long *) first);
+  queue_print(queue_ulong);
+
+  printf("\n*** Polling one element of the queue ***\n");
+  first = queue_poll(queue_ulong);
+  printf("Polling queue's first element: %lu\n", *(unsigned long *) first);
+  queue_print(queue_ulong);
+
+  printf("\n*** Polling one element of the queue ***\n");
+  first = queue_poll(queue_ulong);
+  printf("Polling queue's first element: %lu\n", *(unsigned long *) first);
+  queue_print(queue_ulong);
+
+  queue_destroy(queue_ulong);
+}
+
+void test_long() {
+  Queue_t queue_long = queue_create(LONG); 
+  long n = 42934729;
+  long temp = n;
+  int sign = 1;
+
+  printf("\n*** Adding 10 long elements to the queue ***\n");
+  for(long i = 1; i <= 10; i++) {
+    sign = (i % 2) ? -1 : 1; 
+    temp = n * i * sign;
+    queue_offer(queue_long, (void *)&temp);
+  }
+  queue_print(queue_long);
+
+  printf("\n*** Peeking queue's front ***\n");
+  type_t first = queue_peek(queue_long);
+  printf("Peeking queue's first element: %ld\n", *(long *) first);
+  queue_print(queue_long);
+
+  printf("\n*** Polling one element of the queue ***\n");
+  first = queue_poll(queue_long);
+  printf("Polling queue's first element: %ld\n", *(long *) first);
+  queue_print(queue_long);
+
+  printf("\n*** Polling one element of the queue ***\n");
+  first = queue_poll(queue_long);
+  printf("Polling queue's first element: %ld\n", *(long *) first);
+  queue_print(queue_long);
+
+  queue_destroy(queue_long);
+}
+
 void test_float();
 void test_double();
 void test_string();

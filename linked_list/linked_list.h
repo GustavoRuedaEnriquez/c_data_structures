31ae52d2e905d880097d0cea5309333b4c1a7496
GustavoRuedaEnriquez <grueda-personal@outlook.com>
2024-11-29 17:04:31 -0600

    Removed not feasable functions

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -12,21 +12,17 @@ typedef struct Linked_List_str * Linked_List_t;
 
 Linked_List_t linked_list_create(datatype_t);
 bool_t        linked_list_destroy(Linked_List_t);
-
 bool_t        linked_list_add(Linked_List_t, type_t);
 bool_t        linked_list_add_front(Linked_List_t, type_t);
 bool_t        linked_list_add_back(Linked_List_t, type_t);
-//bool_t        linked_list_add_all(Linked_List_t, type_t*)
 bool_t        linked_list_set(Linked_List_t, unsigned int, type_t);
 type_t        linked_list_get_index(Linked_List_t, unsigned int);
 type_t        linked_list_get_front(Linked_List_t);
 type_t        linked_list_get_back(Linked_List_t);
 int           linked_list_index_of(Linked_List_t, type_t);
-//type_t*       linked_list_to_array(linked_list_t);
 bool_t        linked_list_remove_index(Linked_List_t, unsigned int);
 bool_t        linked_list_remove_front(Linked_List_t);
 bool_t        linked_list_remove_back(Linked_List_t);
-bool_t        linked_list_remove_element(Linked_List_t, type_t);
 bool_t        linked_list_clear(Linked_List_t);
 int           linked_list_size(Linked_List_t);
 bool_t        linked_list_is_empty(Linked_List_t);

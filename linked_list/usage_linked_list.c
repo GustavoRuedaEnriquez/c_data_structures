6f0d20b015bc6f5fd887b2077ad118401abffc39
GustavoRuedaEnriquez <grueda-personal@outlook.com>
2024-12-02 21:47:04 -0600

    Add support for unsigned long datatypes

--- linked_list/usage_linked_list.c
+++ linked_list/usage_linked_list.c
@@ -9,7 +9,7 @@ Nov 23rd 2024
 
 void test_uint();
 void test_int();
-//void test_ulong();
+void test_ulong();
 //void test_long();
 //void test_float();
 //void test_double();
@@ -25,13 +25,13 @@ int main() {
   test_int();
 
   printf("\n\n\n");*/
-/*
+
   printf("====== Testing queue for unsigned longs ======\n");
   test_ulong();
 
   printf("\n\n\n");
 
-  printf("====== Testing queue for longs ======\n");
+  /*printf("====== Testing queue for longs ======\n");
   test_long();
 
   printf("\n\n\n");
@@ -44,12 +44,12 @@ int main() {
   printf("====== Testing queue for doubles ======\n");
   test_double();
 
-  printf("\n\n\n");*/
+  printf("\n\n\n");
 
   printf("====== Testing queue for strings ======\n");
   test_string();
 
-  printf("\n\n\n");
+  printf("\n\n\n");*/
 
     return 0;
 }
@@ -196,6 +196,82 @@ void test_int() {
   linked_list_print(list_int);
   
   linked_list_destroy(list_int);
+}
+
+void test_ulong() {
+  Linked_List_t list_ulong = linked_list_create(DATATYPE_ULONG);
+  unsigned long r = 0;
+
+  printf("\n*** Adding 10 unsigned longs elements to the linked list ***\n");
+  for(int i = 0; i < 10; i++) {
+    r = rand() % (40000000 - 0 + 1) + 0;
+    linked_list_add(list_ulong, (void *)&r);
+  }
+
+  type_t front = linked_list_get_front(list_ulong);
+  type_t back = linked_list_get_back(list_ulong);
+  printf("Front: %lu\tBack: %lu\n", *(unsigned long *)front, *(unsigned long *)back);
+  linked_list_print(list_ulong);
+
+  printf("\n*** Changing the value of the 4th element ***\n");
+  unsigned long value = 5687634;
+  linked_list_set(list_ulong, 4, (void *)&value);
+  front = linked_list_get_front(list_ulong);
+  back = linked_list_get_back(list_ulong);
+  printf("Front: %lu\tBack: %lu\n", *(unsigned long *)front, *(unsigned long *)back);
+  linked_list_print(list_ulong);
+
+  printf("\n*** Adding a value to the front of the linked list ***\n");
+  value = 120988;
+  linked_list_add_front(list_ulong, (void *)&value);
+  front = linked_list_get_front(list_ulong);
+  back = linked_list_get_back(list_ulong);
+  printf("Front: %lu\tBack: %lu\n", *(unsigned long *)front, *(unsigned long *)back);
+  linked_list_print(list_ulong);
+
+  printf("\n*** Adding a value to the back of the linked list ***\n");
+  value = 63453;
+  linked_list_add_back(list_ulong, (void *)&value);
+  front = linked_list_get_front(list_ulong);
+  back = linked_list_get_back(list_ulong);
+  printf("Front: %lu\tBack: %lu\n", *(unsigned long *)front, *(unsigned long *)back);
+  linked_list_print(list_ulong);
+  
+  printf("\n*** Getting the 7th element of the list ***\n");
+  type_t search = linked_list_get_index(list_ulong, 7);
+  printf("7th element contains: %lu\n", *(unsigned long *)search);
+
+  printf("\n*** Searching for the element that has value of 56 ***\n");
+  value = 5687634;
+  int index = linked_list_index_of(list_ulong, (void *)&value);
+  printf("Desired node (%lu) is on index %d\n", value, index);
+
+  printf("\n*** Deleting 1st element of the list ***\n");
+  linked_list_remove_front(list_ulong);
+  front = linked_list_get_front(list_ulong);
+  back = linked_list_get_back(list_ulong);
+  printf("Front: %lu\tBack: %lu\n", *(unsigned long *)front, *(unsigned long *)back);
+  linked_list_print(list_ulong);
+
+  printf("\n*** Deleting last element of the list ***\n");
+  linked_list_remove_back(list_ulong);
+  front = linked_list_get_front(list_ulong);
+  back = linked_list_get_back(list_ulong);
+  printf("Front: %lu\tBack: %lu\n", *(unsigned long *)front, *(unsigned long *)back);
+  linked_list_print(list_ulong);
+
+  printf("\n*** Deleting 6th element of the list ***\n");
+  linked_list_remove_index(list_ulong, 6);
+  front = linked_list_get_front(list_ulong);
+  back = linked_list_get_back(list_ulong);
+  printf("Front: %lu\tBack: %lu\n", *(unsigned long *)front, *(unsigned long *)back);
+  linked_list_print(list_ulong);
+
+  printf("\n*** Clearing the list ***\n");
+  linked_list_clear(list_ulong);
+  linked_list_print(list_ulong);
+
+  linked_list_destroy(list_ulong);
 }
 
 void test_string() {

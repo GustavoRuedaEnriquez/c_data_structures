f7240c58cb5cee299423576f649c944a3c4c3c53
GustavoRuedaEnriquez <grueda-personal@outlook.com>
2024-12-08 21:03:22 -0600

    Add useful comments to header and c files.

--- stack/stack.c
+++ stack/stack.c
@@ -8,108 +8,129 @@ May 16th 2022
 #include<stdlib.h>
 #include <string.h>
 
-struct Stack_Node_str {
+struct Stack_Node_struct {
   type_t data;
-  struct Stack_Node_str *prior;
+  struct Stack_Node_struct *prior;
 };
-struct Stack_str {
+
+struct Stack_struct {
   Stack_Node_t top;
-  int size;
+  unsigned int size;
   datatype_t datatype;
 };
 
+
 Stack_t stack_create(datatype_t type) {
-  Stack_t s = (Stack_t) calloc(1, sizeof(struct Stack_str));
-  memset(s, 0, sizeof(struct Stack_str));
+  // Allocate memory for a new stack struct.
+  Stack_t s = (Stack_t) calloc(1, sizeof(struct Stack_struct));
+  memset(s, 0, sizeof(struct Stack_struct));
+
+  // Fill stack's attributes
   s->datatype = type;
+  s->size = 0;
+  s->top = NULL;
   return s;
 }
 
-int stack_size(Stack_t s) {
-  return s->size;
+
+void stack_push(Stack_t s, type_t data) {
+  // Allocate memory for a new node.
+  Stack_Node_t new_node = (Stack_Node_t) malloc(sizeof(struct Stack_struct));
+  memset(new_node, 0, sizeof(struct Stack_struct));
+
+  // Allocate memory for the data the new node will contain.
+  new_node->data = malloc(GET_DATA_SIZE(s->datatype, data));
+  memset(new_node->data, 0, GET_DATA_SIZE(s->datatype, data));
+  memcpy(new_node->data, data, GET_COPY_DATA_SIZE(s->datatype, data));
+  
+  // Update stack's top.
+  new_node->prior = s->top;
+  s->top = new_node;
+  s->size++;
 }
 
-bool_t stack_isEmpty(Stack_t s) {
-  if (s->top == 0) {
-    return TRUE;
-  }
-  else{
-    return FALSE;
-  }
-}
 
 type_t stack_top(Stack_t s) {
+  // If top == NULL, return NULL.
   if (s->top != NULL) {
     return s->top->data;
   }
   return NULL;
 }
 
-void stack_push(Stack_t s, type_t data) {
-  Stack_Node_t new_node = (Stack_Node_t) malloc(sizeof(struct Stack_Node_str));
-  memset(new_node, 0, sizeof(struct Stack_Node_str));
-
-  new_node->data = malloc(GET_DATA_SIZE(s->datatype, data));
-  memset(new_node->data, 0, GET_DATA_SIZE(s->datatype, data));
-  memcpy(new_node->data, data, GET_COPY_DATA_SIZE(s->datatype, data));
-  
-  new_node->prior = NULL;
-
-  new_node->prior = s->top;
-  s->size++;
-  s->top = new_node;
-}
 
 type_t stack_pop(Stack_t s) {
-  type_t data;
+  // If stack is empty, return NULL
   if (s->size == 0) {
     return NULL;
   }
+
+  // Save top's data 
+  type_t data;
   data = s->top->data;
+
+  // Remove stack's top and set new top entry.
   s->size--;
   s->top = s->top->prior;
   return data;
 }
 
+
+unsigned int stack_size(Stack_t s) {
+  return s->size;
+}
+
+
+bool_t stack_is_empty(Stack_t s) {
+  return (s->size == 0);
+}
+
+
 void stack_destroy(Stack_t s) {
   Stack_Node_t temp;
+
+  // Free each of the stack's nodes.
   while (s->top != NULL) {
     temp = s->top->prior;
+    free(s->top->data);
     free(s->top);
+    s->size--;
     s->top = temp;
   }
+
+  // Finally, free the memory of the stack structure.
   free(s);
   return;
 }
 
+
 void stack_print(Stack_t s) {
   if (s->size == 0)
-    printf("The stack is empty\n");
+    printf("Stack is empty\n");
   else {
     if (s->datatype == DATATYPE_MIXED)
       printf("Stack's data is mixed, unable to fully print its contents\n");
     else {
+      // Depending of the datatype of the stack, use the right print format.
       Stack_Node_t temp = s->top;
       printf("Stack's size: %d\n", s->size);
       while (temp != NULL) {
-        printf("---\n");
         if (s->datatype == DATATYPE_UINT)
-          printf("|%u\n", VOID_PTR_2_UINT(temp->data));
+          printf("%u\n", VOID_PTR_2_UINT(temp->data));
         else if (s->datatype == DATATYPE_INT)
-          printf("|%d\n", VOID_PTR_2_INT(temp->data));
+          printf("%d\n", VOID_PTR_2_INT(temp->data));
         else if (s->datatype == DATATYPE_ULONG)
-          printf("|%lu\n", VOID_PTR_2_ULONG(temp->data));
+          printf("%lu\n", VOID_PTR_2_ULONG(temp->data));
         else if (s->datatype == DATATYPE_LONG)
-          printf("|%ld\n", VOID_PTR_2_LONG(temp->data));
+          printf("%ld\n", VOID_PTR_2_LONG(temp->data));
         else if (s->datatype == DATATYPE_FLOAT)
-          printf("|%.6f\n", VOID_PTR_2_FLOAT(temp->data));
+          printf("%.6f\n", VOID_PTR_2_FLOAT(temp->data));
         else if (s->datatype == DATATYPE_DOUBLE)
-          printf("|%.6f\n", VOID_PTR_2_DOUBLE(temp->data));
+          printf("%.6f\n", VOID_PTR_2_DOUBLE(temp->data));
         else if (s->datatype == DATATYPE_STRING)
-          printf("|%s\n", temp->data);
+          printf("%s\n", temp->data);
         temp = temp->prior;
       }
-      printf("---\n");
     }
   }
   return;
--- stack/stack.h
+++ stack/stack.h
@@ -7,16 +7,138 @@ May 16th 2022
 #ifndef STACK_H_
 #define STACK_H_
 
-typedef struct Stack_Node_str  * Stack_Node_t;
-typedef struct Stack_str * Stack_t;
+/* Structure the represents a stack node, it has 2 attributes:
+ * - type_t data: Information that will be stored on the stack node.
+ * - Stack_Node_t prior: Pointer to the previous node.
+ */
+typedef struct Stack_Node_struct * Stack_Node_t;
 
+/* Structure the represents a stack, it has 3 attributes:
+ * - Stack_Node_t top: Pointer to the stack's top entry.
+ * - unsigned int size: Current stack's size.
+ * - datatype_T datatype: Enum that represents the datatype of the entries
+ *                        stored on the stack.
+ */
+typedef struct Stack_struct * Stack_t;
+
+/* FUNCTION:
+ * stack_create
+ *
+ * DESCRIPTION:
+ * Function used to initialize a stack. Returned stack will have a size of 0
+ * and it's top will equal to NULL.
+ *
+ * PARAMETERS:
+ * datatype_t - Indicates the data type that will be stored on the stack.
+ *
+ * RETURNS:
+ * Stack_t - Pointer to the recently created stack structure.
+ */
 Stack_t stack_create(datatype_t);
-int     stack_size(Stack_t);
-bool_t  stack_isEmpty(Stack_t);
-type_t  stack_top(Stack_t);
-void    stack_push(Stack_t, type_t);
-type_t  stack_pop(Stack_t);
-void    stack_destroy(Stack_t);
-void    stack_print(Stack_t);
+
+
+/* FUNCTION:
+ * stack_push
+ *
+ * DESCRIPTION:
+ * Adds new entry to the stack's top.
+ *
+ * PARAMETERS:
+ * Stack_t - Stack structure which we want to add an entry.
+ * type_t  - Data entry to add to the stack. 
+ *
+ * RETURNS:
+ * Nothing
+ */
+void stack_push(Stack_t, type_t);
+
+
+/* FUNCTION:
+ * stack_top
+ *
+ * DESCRIPTION:
+ * Returns the stack's top. This function DOES NOT removes the top element.
+ *
+ * PARAMETERS:
+ * Stack_t - Stack structure from which we want to obtain the top element.
+ *
+ * RETURNS:
+ * type_t - Entry at stack's top.
+ */
+type_t stack_top(Stack_t);
+
+
+/* FUNCTION:
+ * stack_pop
+ *
+ * DESCRIPTION:
+ * Removes the stack's top and returns it.
+ *
+ * PARAMETERS:
+ * Stack_t - Stack structure from which we want to remove the top element.
+ *
+ * RETURNS:
+ * type_t - Entry at stack's top.
+ */
+type_t stack_pop(Stack_t);
+
+/* FUNCTION:
+ * stack_size
+ *
+ * DESCRIPTION:
+ * Returns current size of the stack.
+ *
+ * PARAMETERS:
+ * Stack_t - Stack structure from which we want to obtain the size.
+ *
+ * RETURNS:
+ * unsigned int - Current size of the stack.
+ */
+unsigned int stack_size(Stack_t);
+
+
+/* FUNCTION:
+ * stack_is_empty
+ *
+ * DESCRIPTION:
+ * Tells if stack is empty or not.
+ *
+ * PARAMETERS:
+ * Stack_t - Stack structure from which we want to know if it is empty.
+ *
+ * RETURNS:
+ * bool_t - TRUE/FALSE depending if stack's empty or not.
+ */
+bool_t stack_is_empty(Stack_t);
+
+
+/* FUNCTION:
+ * stack_destroy
+ *
+ * DESCRIPTION:
+ * Frees the memory used by a stack structure.
+ *
+ * PARAMETERS:
+ * Stack_t - Stack structure to destroy. 
+ *
+ * RETURNS:
+ * Nothing
+ */
+void stack_destroy(Stack_t);
+
+
+/* FUNCTION:
+ * stack_print
+ *
+ * DESCRIPTION:
+ * Prints all the stack's entries.
+ *
+ * PARAMETERS:
+ * Stack_t - Stack structure to print. 
+ *
+ * RETURNS:
+ * Nothing
+ */
+void stack_print(Stack_t);
 
 #endif /* STACK_H_ */

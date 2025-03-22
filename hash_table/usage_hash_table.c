273e63da3dab7b1fbe7722133f5d1b11301a3714
GustavoRuedaEnriquez <grueda-personal@outlook.com>
2025-03-21 22:04:36 -0600

    Fix typos

--- hash_table/usage_hash_table.c
+++ hash_table/usage_hash_table.c
@@ -22,18 +22,16 @@ int main() {
   */
   HashTable_t ht = hashtable_create(DATATYPE_STRING, DATATYPE_STRING);
   char *keys[] = { "key 1", "key 2", "key 3", "key 4", "key 5", \
-                   "key 6", "key 7", "key 8", "Key 9", "key 10", \
+                   "key 6", "key 7", "key 8", "key 9", "key 10", \
                    "key 11", "key 12", "key 13", "key 14", "key 15", \
                    "key 16", "key 17", "key 18", "key 19", "key 20" };
   char *values[] = { "value 1", "value 2", "value 3", "value 4", "value 5", \
-                     "value 6", "value 7", "value 8", "Key 9", "value 10", \
+                     "value 6", "value 7", "value 8", "value 9", "value 10", \
                      "value 11", "value 12", "value 13", "value 14", "value 15", \
                      "value 16", "value 17", "value 18", "value 19", "value 20" };
 
   for (int i = 0; i < 20; i++) {
     hashtable_insert(ht,(void *)keys[i], (void *)values[i]);
-    //hashtable_print(ht);
-    //printf("\n");
   }
   hashtable_print(ht);
 
